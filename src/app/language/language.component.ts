0ea0e6791d451afa48fce084da92f2d8873e96b0
ryzas228 <stasryzov1022@gmail.com>
2023-02-25 16:40:11 +0200

    initial commit

---
+++ src/app/language/language.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './language.component.html',
+  styleUrls: ['language.component.css']
+})
+export class AppComponent {
+  title = 'my-angular-app';
+}
+
+
